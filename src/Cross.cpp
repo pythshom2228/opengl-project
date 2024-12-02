e80f0328e46ee7e8b4e63cf804e086f94ebf6173
pythshoma228 <sh.magomedov05@mail.ru>
2024-12-02 21:50:42 +0300

    Cross implementation class

---
+++ src/Cross.cpp
@@ -0,0 +1,24 @@
+#include "include/Cross.h"
+
+ const std::array<float,16> Cross::vertexes = {
+    0.5f,0.1f,
+    -0.5f,0.1f, 
+    -0.5f,-0.1f,
+    0.5f,-0.1f,
+    
+    0.1f,0.6f, 
+    -0.1f,0.6f, 
+    -0.1f,-0.6f, 
+    0.1f,-0.6f
+};
+
+
+const std::array<unsigned int, 12> Cross::indices = {
+    0,1,2,
+    0,2,3,
+
+    4,5,6,
+    4,6,7
+};
+
+decltype(Cross::buffer) Cross::buffer = {};
