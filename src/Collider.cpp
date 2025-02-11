f7224b025a53447714cdac65f1f9ff2d2def5e5c
pythshom2228 <sh.magomedov05@mail.ru>
2025-02-11 20:23:35 +0300

    collider class added

---
+++ src/Collider.cpp
@@ -0,0 +1,14 @@
+#include "include/Collider.h"
+
+
+Collider::Collider(const std::array<std::int32_t,2> scale) : _scale(scale) {}
+
+void Collider::update(const glm::vec3 verticesPos) {
+   
+}
+
+
+bool Collider::checkIfCollides(const Collider& other) const {
+    
+}
+  
