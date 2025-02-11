a2f47a057668af131ae35c5da98431d6e7ce1c4f
pythshom2228 <sh.magomedov05@mail.ru>
2025-02-11 20:31:49 +0300

    added quick inventory

--- src/include/Player.h
+++ src/include/Player.h
@@ -10,27 +10,60 @@
 
 class Player {
 public:
+    enum gameMode {
+        SURVIVAL = 0,
+        CREATIVE
+    } gamemode;
+
     Player();
 
     ~Player() = default;
 
-    void processInput();
+    void update(const sf::Window& window,std::vector<Cube>& _cube);
 
-    void handleMouseMoves(const sf::Window& window);
 
     void setMousePos(sf::Vector2i position);
 
     void setPosition(glm::vec3 position);
+
+    const Cube::types& getCurrentHoldingCube();
 
     glm::vec3 getPosition();
 
     Camera camera;
 
 
-    float sensitivity = 0.1f;
+    float sensitivity = 1.0f;
     
 
 private:
+    glm::vec3 lookVector = this->getPosition() + glm::vec3(camera.cameraFront.x*3,camera.cameraFront.y*3,camera.cameraFront.z*3);
+
+    Cube& lookingAt(std::vector<Cube>& _cubes);
+
+    void processInput(std::vector<Cube>& _cubes);
+    void handleMouseMoves(const sf::Window& window);
+
+    void putCube(std::vector<Cube>& _cubes);
+    void destructCube();
+
+    std::uint8_t _cubeDestructionProgress = 0;  
+
+    std::array<Cube::types, 9> _quickInventory = {
+        Cube::types::DIRT,
+        Cube::types::GRASS,
+        Cube::types::COBBLESTONE,
+        Cube::types::PLANKS,
+        Cube::types::NONE,
+        Cube::types::NONE,
+        Cube::types::NONE,
+        Cube::types::NONE,
+        Cube::types::NONE
+    };
+    //std::reference_wrapper<Cube> _currentLookingCube;
+
+    Cube::types _currentHoldingCube = _quickInventory[0];
+
     glm::vec3 _position  = glm::vec3(0.0f,0.0f,0.0f);
 
     
